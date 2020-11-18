fe15704f2300429beb40107d2472e17675aa33fc
Polina-Prakhova <polinaprakhova@gmail.com>
2020-11-18 13:30:09 +0200

    Add views, reconstruct templates

---
+++ department-app/views/__init__.py
@@ -0,0 +1,6 @@
+from flask import Blueprint
+
+template_folder = 'templates'
+departments_page = Blueprint('departments', __name__, template_folder=template_folder)
+employees_page = Blueprint('employees', __name__, template_folder=template_folder)
+employee_page = Blueprint('employee', __name__, template_folder=template_folder)
--- department-app/views/departments.py
+++ department-app/views/departments.py
@@ -1,17 +1,10 @@
-from flask import render_template, Blueprint
-from ..models.models import Department, Employee
+from flask import render_template
 
-departments_page = Blueprint('departments_page', __name__,
-                             template_folder='templates')
+from . import departments_page
 
 
 @departments_page.route('/')
 @departments_page.route('/index')
 @departments_page.route('/departments')
 def show():
-    lines = [{'name': 'dfasfasf', 'employees': 22, 'salary': 1},
-             {'name': 'dfasfasf', 'employees': 22, 'salary': 1},
-             {'name': 'dfasfasf', 'employees': 22, 'salary': 1},
-             {'name': 'dfasfasf', 'employees': 22, 'salary': 1}]
-    return render_template('departments.html', title='Departments',
-                           headers=Department.query.all(), departments=Employee.query.all())
+    return render_template('departments.html', title='Departments')
