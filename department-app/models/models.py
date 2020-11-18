f2fab820d3094b3380a4969bd76a383326002749
Polina-Prakhova <polinaprakhova@gmail.com>
2020-11-18 13:33:16 +0200

    Add models

--- department-app/models/models.py
+++ department-app/models/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -8,9 +10,14 @@ class Department(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), unique=True, nullable=False)
- #   employees = db.relationship('Employee', backref='departments', lazy=True)
-  #  number_of_employees = Column(Integer, nullable=False)
-  #  average_salary = Column(Float, nullable=False)
+
+    def create(self):
+        db.session.add(self)
+        db.session.commit()
+        return self
+
+    def __init__(self, name: str):
+        self.name = name
 
     def __repr__(self):
         return '<Department %r>' % self.name
@@ -26,5 +33,28 @@ class Employee(db.Model):
     department_id = db.Column(db.Integer, db.ForeignKey('departments.id'))
     department = db.relationship('Department', backref='employees', primaryjoin=department_id == Department.id)
 
+    def __init__(self, name: str, birthday: datetime, salary: float, department_id: int):
+        self.name = name
+        self.birthday = birthday
+        self.salary = salary
+        self.department_id = department_id
+
     def __repr__(self):
         return f'<Employee {self.name} {self.birthday} {self.salary} >'
+
+
+class DepartmentInfo(db.Model):
+    __tablename__ = 'department_info'
+
+    id = db.Column(db.Integer, db.ForeignKey('departments.id'), primary_key=True)
+    department = db.relationship('Department', backref='department_info', primaryjoin=id == Department.id)
+    avg_salary = db.Column(db.Float, nullable=False)
+    count_employees = db.Column(db.Integer, nullable=False)
+
+    def __init__(self, department_id: int, avg_salary: float, count_employees: int):
+        self.department_id = department_id
+        self.avg_salary = avg_salary
+        self.count_employees = count_employees
+
+    def __repr__(self):
+        return f'<Department №{self.id} {self.avg_salary} {self.count_employees}>'
